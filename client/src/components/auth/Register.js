cfb7fade58685df40264ec7b3b8ea518abf33fad
ariesastra <ariesastra@windowslive.com>
2021-12-23 20:36:35 +0700

    finish layouting login

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import server from '../../apis/serverApi'
 
 const Register = () => {
@@ -91,7 +92,7 @@ const Register = () => {
           <input type="submit" className="btn btn-primary" value="Register" />
         </form>
         <p className="my-1">
-          Already have an account? <a href="login.html">Sign In</a>
+          Already have an account? <Link to="/login">Sign In</Link>
         </p>
     </div>
   )
