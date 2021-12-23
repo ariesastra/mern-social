41e17c899bfadd6c24db0a697cb0cf45169242ac
ariesastra <ariesastra@windowslive.com>
2021-12-23 21:39:40 +0700

    finish add alert reducer

---
+++ client/src/actions/alert.js
@@ -0,0 +1,17 @@
+import uuid from 'uuid'
+import { 
+  SET_ALERT, 
+  REMOVE_ALERT 
+} from './types'
+
+export const setAlert = (message, alertType) => dispatch => {
+  const id = uuid.v5()
+  dispatch({
+    type: SET_ALERT,
+    payload: {
+      message,
+      alertType,
+      id
+    }
+  })
+}
